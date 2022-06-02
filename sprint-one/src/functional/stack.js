42bc88f0da53df2054ed700ca76c05191c6e2911
Cheyenne Cornett <cheyenne.cornett22@gmail.com>
2022-06-02 17:11:31 -0400

    Finished Functional Stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,32 @@
 var Stack = function() {
   var someInstance = {};
-
+  // push(string) - Add a string to the top of the stack
+  // pop() - Remove and return the string on the top of the stack
+  // size() - Return the number of items on the stack
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var spot = 0;
+
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[spot] = value;
+    spot++;
+
   };
 
   someInstance.pop = function() {
+    if (spot > 0) {
+      spot--;
+      var result = storage[spot];
+      delete storage[spot];
+      return result;
+
+    }
   };
 
   someInstance.size = function() {
+    return spot;
   };
 
   return someInstance;
