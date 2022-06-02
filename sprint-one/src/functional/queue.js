4d7848546bee9f514bae46f88b11f777e3ba7fdb
Cheyenne Cornett <cheyenne.cornett22@gmail.com>
2022-06-02 16:48:29 -0400

    Revert "Finished Functional Queue"
    This reverts commit dfd943e2e3abe7d22c14988064da27b92e50a750.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,26 +1,18 @@
 var Queue = function() {
   var someInstance = {};
+
+  // Use an object with numeric keys to store values
   var storage = {};
-  var front = 0;
-  var back = 0; 
+
+  // Implement the methods below
 
   someInstance.enqueue = function(value) {
-    storage[front] = value;
-    front ++;
   };
 
   someInstance.dequeue = function() {
-    
-  if ( front - back) {
-    var result = storage[back];
-    delete storage[back];
-    back++;
-    return result;
-    }
   };
 
   someInstance.size = function() {
-    return front - back;
   };
 
   return someInstance;
