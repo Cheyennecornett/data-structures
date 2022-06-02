dbec87ac939c73722b313c570bc2de9fc8719e78
Cheyenne Cornett <cheyenne.cornett22@gmail.com>
2022-06-02 18:48:11 -0400

    Finished Functional Shared Stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,36 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    storage: {},
+    spot: 0
+  };
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.spot] = value;
+    this.spot++;
+
+  },
+  pop: function() {
+    if (this.spot > 0) {
+      this.spot--;
+      var result = this.storage[this.spot];
+      delete this.storage[this.spot];
+      return result;
+    }
+  },
+  size: function() {
+    return this.spot;
+  }
+
+
+
+
+};
 
 
