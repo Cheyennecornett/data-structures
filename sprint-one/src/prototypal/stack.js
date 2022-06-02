a900aaf81977d11dc3e1b30197d520aea8e68485
Cheyenne Cornett <cheyenne.cornett22@gmail.com>
2022-06-02 19:44:13 -0400

    Finished Proto Stack and Proto Queue. (Forgot to commit sooner)

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
+
+  var someInstance = Object.create(stackMethods);
+
+  someInstance.storage = {};
+  someInstance.spot = 0;
+
+  return someInstance;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.spot] = value;
+    this.spot++;
+  },
+  pop: function() {
+    if (this.spot > 0) {
+      this.spot--;
+      var result = this.storage[this.spot];
+      delete this.storage[this.spot];
+      return result;
+    }
+  },
+  size: function() {
+    return this.spot;
+  }
+};
 
 
