bae384b082a7b749a80245ccaae8f3f4aefbd41d
Cheyenne Cornett <cheyenne.cornett22@gmail.com>
2022-06-04 08:20:50 -0400

    Finished e6 Stack and Queue

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,28 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.spot = 0;
   }
+
+  push (value) {
+    this.storage[this.spot] = value;
+    this.spot++;
+
+  };
+
+  pop () {
+  if (this.spot > 0) {
+    this.spot--;
+    var result = this.storage[this.spot];
+    delete this.storage[this.spot];
+    return result;
+
+  }
+};
+
+  size () {
+  return this.spot;
+};
 
 }
