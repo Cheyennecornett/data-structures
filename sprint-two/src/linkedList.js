cb0d6259bada4e0baec3a3f479400d36e1cb7f10
Cheyenne Cornett <cheyenne.cornett22@gmail.com>
2022-06-04 08:12:59 -0400

    Finished Linked List and Tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,16 +4,39 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newTail = Node(value);
+    if (list.tail === null) {
+      list.head = newTail;
+      list.tail = newTail;
+    } else {
+      list.tail.next = newTail;
+      list.tail = newTail;
+    }
   };
 
   list.removeHead = function() {
+    removed = list.head.value;
+    list.head = list.head.next;
+    return removed;
   };
 
   list.contains = function(target) {
+    var current = this.head;
+    while (current !== null) {
+      if (current.value === target) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
   };
 
   return list;
 };
+
+
+
+
 
 var Node = function(value) {
   var node = {};
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,22 +1,46 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
+  //console.log(newTree.value);
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
+//_.extend(newTree, treeMethods);
+
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var node = Tree(value);
+  //console.log('node:', node);
+  this.children.push(node);
+
+
+
 };
+
 
 treeMethods.contains = function(target) {
+  var result = false;
+  var search = function (node) {
+    if (node.value === target) {
+      result = true;
+    } else {
+      _.each(node.children, function(child) {
+        search(child);
+      });
+    }
+  };
+  search(this);
+  return result;
+
 };
 
-
+treeMethods.contains(4);
 
 /*
  * Complexity: What is the time complexity of the above functions?
